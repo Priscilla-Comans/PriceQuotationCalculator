8aff58be548ad8f75fca98c0355684190ff871eb
Priscilla-Comans <64997063+Priscilla-Comans@users.noreply.github.com>
2022-05-10 14:16:07 -0500

    Add files via upload

---
+++ PriceQuotation/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PriceQuotation.Models;
+
+namespace PriceQuotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+          
+            ViewBag.FV = 0;
+            ViewBag.Result = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateDiscountAmount();
+                ViewBag.Result = model.CalculateTotal();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+                ViewBag.Result = 0;
+            }
+            return View(model);
+        }
+    }
+}
